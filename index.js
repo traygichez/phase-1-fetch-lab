6e28d93e1229c94d384b3e6e16b044acd089c390
tray <onesimus.githua@student.moringaschool.com>
2023-10-01 13:57:06 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => {
+      if (!response.ok) {
+               Error('response not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+              renderBooks(data);
+                  console.log(data);
+    })
+    .catch(error => {
+                 console.error('Error fetching data:', error);
+    });
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
+  const bookList = document.getElementById('book-list');
   books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+    const listItem = document.createElement('li');
+    listItem.innerHTML = book.name;
+    bookList.appendChild(listItem);
   });
 }
-
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
